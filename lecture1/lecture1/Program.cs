c80e623a4451d0f516378f67246099ae711a7450
Usama <Usama@aptechnn2.edu>
2024-03-28 10:13:22 +0500

    Variables completed

---
+++ lecture1/lecture1/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Dusr\0a Text");
+
+
+int num = 12;
+num = 45;
+Console.WriteLine(num);
+
+string alpha = "Usama";
+alpha = "sdfsad";
+Console.WriteLine(alpha);
+
+double p = 2.3;
+Console.WriteLine(p);
+
+var a = "dfdfsad";
+a = "sadf";
+Console.WriteLine(a);
+
+const double pi = 3.14;
+//pi = 2.2;
+Console.WriteLine(pi);
+
+
+
+
+
+
+Console.ReadKey();
